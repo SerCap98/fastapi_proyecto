89469f32c5482e3fc76c1c1538ad730cec39cc7d
Sergio Capon Blanco <scapon.17@est.ucab.edu.ve>
2024-01-11 17:54:15 -0400

    Finalizada creacion y migracion de tablas
    Finalizada la implementacion de las tablas restantes del sistema con la libreria Alembic

--- backend/alembic/versions/84bd788205e0_create_production_material_usage_table.py
+++ backend/alembic/versions/84bd788205e0_create_production_material_usage_table.py
@@ -5,8 +5,10 @@ Revises: b58cc44e6aeb
 Create Date: 2024-01-11 15:04:41.105151
 
 """
+from uuid import uuid4
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import UUID
 
 
 # revision identifiers, used by Alembic.
@@ -16,9 +18,22 @@ branch_labels = None
 depends_on = None
 
 
-def upgrade() -> None:
-    pass
+def create_production_material_usage_table():
+    op.create_table(
+        "production_material_usage",
+        sa.Column("id", UUID, primary_key=True, default=uuid4()),
+        sa.Column("id_manufactured_product", UUID, sa.ForeignKey("manufactured_product.id"), nullable=False),
+        sa.Column("id_factory_inventory", UUID, sa.ForeignKey("factory_raw_material_inventory.id"), nullable=False),
+        sa.Column("quantity", sa.Float, nullable=False),        
+        sa.Column("created_by", UUID(as_uuid=True), nullable=False),
+        sa.Column("created_at", sa.DateTime, nullable=False),
+        sa.Column("updated_by", UUID(as_uuid=True), nullable=True),
+        sa.Column("updated_at", sa.DateTime, nullable=True)
+    )
 
+def upgrade() -> None:
+    create_production_material_usage_table()
 
 def downgrade() -> None:
-    pass
+    op.drop_table("production_material_usage")
+
--- backend/alembic/versions/b58cc44e6aeb_create_manufactured_product_table.py
+++ backend/alembic/versions/b58cc44e6aeb_create_manufactured_product_table.py
@@ -5,8 +5,10 @@ Revises: 5dc5b2323897
 Create Date: 2024-01-11 15:04:18.252984
 
 """
+from uuid import uuid4
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import UUID
 
 
 # revision identifiers, used by Alembic.
@@ -15,10 +17,23 @@ down_revision = '5dc5b2323897'
 branch_labels = None
 depends_on = None
 
+def create_manufactured_product_table():
+    op.create_table(
+        "manufactured_product",
+        sa.Column("id", UUID, primary_key=True, default=uuid4()),
+        sa.Column("id_product", UUID, sa.ForeignKey("product.id"), nullable=False),
+        sa.Column("lot_number", sa.Text, nullable=False),
+        sa.Column("quantity", sa.Integer, nullable=False),        
+        sa.Column("created_by", UUID(as_uuid=True), nullable=False),
+        sa.Column("created_at", sa.DateTime, nullable=False),
+        sa.Column("updated_by", UUID(as_uuid=True), nullable=True),
+        sa.Column("updated_at", sa.DateTime, nullable=True)
+    )
 
 def upgrade() -> None:
-    pass
+    create_manufactured_product_table()
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table("manufactured_product")
+
