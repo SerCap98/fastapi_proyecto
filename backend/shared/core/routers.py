e8b1e9fb85ca1fed07929b93fba8633b80d825c2
Sergio Capon Blanco <scapon.17@est.ucab.edu.ve>
2024-01-12 11:46:43 -0400

    Parcheando

--- backend/shared/core/routers.py
+++ backend/shared/core/routers.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from modules.users import users_router
 from modules.RawMaterials import raw_materials_router
-from modules.Factory import factory_routes
+from modules.Factory import factory_router
 
 router = APIRouter()
 
 router.include_router(users_router)
 router.include_router(raw_materials_router)
-router.include_router(factory_routes)
+router.include_router(factory_router)
