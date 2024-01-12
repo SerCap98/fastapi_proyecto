e915ab8643a601fa11dee8afcc361fa8ef185757
Sergio Capon Blanco <scapon.17@est.ucab.edu.ve>
2024-01-11 21:01:58 -0400

    ajustes

--- backend/modules/RawMaterials/RawMaterial_sqlstaments.py
+++ backend/modules/RawMaterials/RawMaterial_sqlstaments.py
@@ -1,31 +1,31 @@
 
 
 CREATE_RAW_MATERIAL = """
-    INSERT INTO raw_materials (id, name, code, created_at, updated_at)
+    INSERT INTO raw_material (id, name, code, created_at, updated_at)
     VALUES (:id, :name, :code, :created_at, :updated_at)
     RETURNING id, name, code;
 """
 
 GET_RAW_MATERIAL_BY_CODE = """
     SELECT id, name, code, created_at, updated_at
-    FROM raw_materials
+    FROM raw_material
     WHERE code = :code;
 """
 
 UPDATE_RAW_MATERIAL_BY_CODE = """
-    UPDATE raw_materials
+    UPDATE raw_material
     SET name = :name, code = :code, updated_at = :updated_at
     WHERE code = :code
     RETURNING id, name, code,updated_at;
 """
 
 DELETE_RAW_MATERIAL_BY_CODE = """
-    DELETE FROM raw_materials
+    DELETE FROM raw_material
     WHERE code = :code
     RETURNING code;
 """
 
 LIST_RAW_MATERIALS = """
     SELECT id, name, code, created_at, updated_at
-    FROM raw_materials;
+    FROM raw_material;
 """
